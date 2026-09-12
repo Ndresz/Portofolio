738fb54821a4afc787d3df2058946c1125b1ab4c
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-09-12 15:48:49 +0700

    Remove devIndicators to fix build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    buildActivity: false, // Disables the build indicator
-  },
+  /* config options here */
 };
 
 export default nextConfig;
